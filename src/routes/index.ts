094b47de4b95ffecdd3d84dbb8e4655fc39bcb67
Bima1301 <bima.aji1380@gmail.com>
2024-05-29 23:23:16 +0700

    add vercel

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,11 @@ import authRoutes from "./auth";
 import todoRoutes from "./todo";
 
 const rootRouter: Router = Router();
-
+rootRouter.get('/', (req, res) => {
+     res.status(200).json({
+          message: 'Welcome to the Todo API'
+     })
+})
 rootRouter.use('/auth', authRoutes);
 rootRouter.use('/todos', todoRoutes);
 
